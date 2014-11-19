8c455d253f50b7812af77ad2af795d785f3bb810
Noelkd <nkdoherty@gmail.com>
2014-11-19 10:26:26 +0000

    tabs to spaces

--- src/serv.py
+++ src/serv.py
@@ -12,7 +12,7 @@ import random
 import json
 import urllib2
 
-# GLOBAL TINGS, SHOULD BE A DATABASE TODO: ADD DATABASE 
+# GLOBAL TINGS, SHOULD BE A DATABASE TODO: ADD DATABASE
 clients = []
 active_sessions = {}
 
@@ -25,7 +25,7 @@ class MusicSessionHolder(object):
         self.title = "TEST TEST TEST"
         self.playlist = []
         self.client_info = { "title" : self.title, "playlist" : self.playlist }
-           
+
     def add_user(self, user):
         if len(self.users) < 1:
             logging.info("Adding main user to session {0}"
@@ -37,10 +37,10 @@ class MusicSessionHolder(object):
             user.player = False
 
     def remove_user(self, user):
-        if not user.player:		
-			self.watchers.remove(user)
+        if not user.player:
+            self.watchers.remove(user)
         else:
-			self.users.remove(user)
+            self.users.remove(user)
     def update_title(self, new_title):
         # TODO: FIND OUT HOW YOUR SUPPOSED TO ESCAPE THIS CORRECTLY
         if new_title:
@@ -55,7 +55,7 @@ class MusicSessionHolder(object):
             if client_message["text"] == "SEND ME A NEW SONG":
                 self.next_track()
             if client_message["text"] == "CHANGE THE TITLE":
-                self.update_title(client_message["title"])        
+                self.update_title(client_message["title"])
 
         if client_message["text"] == "ADD THIS SONG, CHEERS LAD":
             self.update_playlist(client_message)
@@ -72,8 +72,8 @@ class MusicSessionHolder(object):
         video_info["postion"] = len(self.playlist) + 1
         self.playlist.append(video_info)
         self.tell_everyone()
-    
-    def get_info_from_id(self, video_id):  
+
+    def get_info_from_id(self, video_id):
         # Youtube api v2 is depreciated TODO: FIND ALT
         # DO I NEED TO ASYNC LOAD THIS PAGE? SHOULD I EVEN DO THIS? IT WORKS...
         url = "http://gdata.youtube.com/feeds/api/videos/"+video_id+"?v=2&alt=json"
@@ -106,15 +106,15 @@ class MusicSessionHolder(object):
 
 
 def get_new_session_id():
-	new_id = str(random.randint(100000000, 999999999))
-	if active_sessions.get(new_id, None):
-		return get_new_session_id()
-	return 1 # new_id
+    new_id = str(random.randint(100000000, 999999999))
+    if active_sessions.get(new_id, None):
+        return get_new_session_id()
+    return 1 # new_id
 
 
 class Application(tornado.web.Application):
     def __init__(self):
-       
+
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
@@ -122,11 +122,11 @@ class Application(tornado.web.Application):
         )
         handlers = [
             (r"/", IndexHandler),
-			(r"/([0-9]+)", MainHandler),
-			(r"/newsession", NewSessionHandler),
+            (r"/([0-9]+)", MainHandler),
+            (r"/newsession", NewSessionHandler),
             (r"/ws/([0-9]+)", UserHandler),
-			(r"/img/(.*)",tornado.web.StaticFileHandler,
-			{"path": "".join((settings["static_path"],"/img"))},),
+            (r"/img/(.*)",tornado.web.StaticFileHandler,
+            {"path": "".join((settings["static_path"],"/img"))},),
         ]
         tornado.web.Application.__init__(self, handlers, **settings)
 
@@ -134,19 +134,19 @@ class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
-class NewSessionHandler(tornado.web.RequestHandler):	
+class NewSessionHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         new_session_id = get_new_session_id()
         active_sessions[str(new_session_id)] = MusicSessionHolder(new_session_id)
         self.redirect("/{0}".format(new_session_id))
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self, *args, **kwargs):
-		if active_sessions.get(self.request.uri[1:]):
-			self.render("main.html")
+    def get(self, *args, **kwargs):
+        if active_sessions.get(self.request.uri[1:]):
+            self.render("main.html")
 
-		else:  # Otherwise send them back to the start
-			self.redirect(r"/")
+        else:  # Otherwise send them back to the start
+            self.redirect(r"/")
 
 class UserHandler(tornado.websocket.WebSocketHandler):
 
